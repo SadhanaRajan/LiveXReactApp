24794edb73457bdbf9d738b78948c22741069935
Sadhana Rajan <sadhu712@gmail.com>
2025-11-15 19:30:32 -0800

    Added more text to test scrollability

--- src/data/pages.js
+++ src/data/pages.js
@@ -6,7 +6,17 @@ export const pages = {
       'Suspendisse potenti. Phasellus facilisis arcu in porttitor convallis. In sed maximus quam, vitae mattis enim.',
       'Praesent egestas lectus magna, a vulputate neque varius vel. Nullam luctus orci a massa feugiat, quis bibendum nibh faucibus.',
       'Morbi et dolor molestie, sodales lacus nec, viverra lectus. Aenean vitae bibendum magna. Etiam maximus sem ac tortor facilisis pharetra.',
-      'In aliquet, erat vitae feugiat ullamcorper, orci tortor tristique leo, non euismod lorem lorem nec sapien.'
+      'In aliquet, erat vitae feugiat ullamcorper, orci tortor tristique leo, non euismod lorem lorem nec sapien.',
+      'Integer congue nisl a libero egestas, eget consequat arcu luctus. Donec semper pharetra nisl, sed vehicula tellus hendrerit eget.',
+      'Vivamus dictum dui et ultrices porta. Etiam ac porta lorem. Sed porta orci nec lectus hendrerit tincidunt.',
+      'Sed vulputate imperdiet viverra. Duis luctus sem at nulla congue consequat. Nunc tempus auctor condimentum.',
+      'Quisque rhoncus ligula tortor, a congue sem scelerisque vel. Vestibulum euismod pharetra enim, ac venenatis felis eleifend non.',
+      'Nam a pharetra enim. Aliquam erat volutpat. Pellentesque eget suscipit mi, non tempor mi.',
+      'Curabitur quis lacinia nisi. Donec feugiat consectetur eros, in fermentum est volutpat id. Sed sagittis porttitor nisl sit amet suscipit.',
+      'Aliquam congue porta velit vitae auctor. Curabitur condimentum luctus sapien sed blandit.',
+      'Mauris aliquet ante at tellus condimentum, eget convallis risus placerat. Integer cursus eleifend nunc eget tincidunt.',
+      'Etiam viverra consequat ipsum, et tincidunt est sagittis vitae. Maecenas interdum gravida ipsum, ut pretium felis.',
+      'Suspendisse auctor vitae eros tempus aliquet. Donec vitae posuere nulla, quis pulvinar tortor.'
     ]
   },
   documents: {
@@ -16,7 +26,17 @@ export const pages = {
       'Donec vehicula ultrices nibh, ut volutpat diam pharetra volutpat. Pellentesque id augue nec orci interdum dignissim.',
       'Mauris nec nisl id nulla blandit malesuada sit amet ac sapien. Integer volutpat in lectus a fermentum.',
       'Nam consequat tempor pharetra. Proin commodo tincidunt ligula, vitae tristique metus faucibus sed.',
-      'Vestibulum at neque ut justo dignissim faucibus non sit amet magna. Sed varius viverra justo et tincidunt.'
+      'Vestibulum at neque ut justo dignissim faucibus non sit amet magna. Sed varius viverra justo et tincidunt.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sollicitudin, erat eu porta aliquam, velit nisl accumsan justo, sed volutpat nibh nisi vel urna.',
+      'Praesent vel tempor dui. Cras imperdiet risus sed sem vehicula pharetra. Vivamus tempor, nulla quis blandit interdum, nibh sem euismod dui, vitae laoreet velit lorem sit amet odio.',
+      'Ut suscipit consectetur nibh, nec fermentum justo gravida sed. Pellentesque id neque sed nulla viverra bibendum et vel dui.',
+      'Duis commodo dolor non sem maximus, eget pulvinar magna dictum. Curabitur ac nibh molestie, pellentesque quam sit amet, faucibus metus.',
+      'Donec a mauris eget ipsum commodo egestas id sit amet orci. Nam auctor lectus sit amet tempus pretium.',
+      'Integer suscipit quam a orci molestie, nec semper ante faucibus. Integer condimentum ante id nibh dignissim ultrices.',
+      'Nullam nec neque vehicula, posuere urna et, sodales leo. Curabitur nec pharetra tortor, eget gravida purus.',
+      'Vestibulum tempor, diam nec cursus scelerisque, velit est egestas sem, sit amet venenatis velit ante ac augue.',
+      'Etiam sollicitudin placerat odio, nec blandit augue aliquet sit amet. Cras vel diam luctus, aliquet nunc a, mollis ex.',
+      'Phasellus quis urna quis ipsum elementum ullamcorper. Donec euismod turpis sit amet orci malesuada ornare.'
     ]
   }
 };
